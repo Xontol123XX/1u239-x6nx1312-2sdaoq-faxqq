527be16af783c85080f674779063a33ea56be39b
Xontol123XX <isakmiracle451@gmail.com>
2025-05-07 23:28:04 +0700

    Delete ArcCas directory

--- ArcCas/test/Tenor/server.py
+++
@@ -1,9 +0,0 @@
-import random
-low = 5
-high = 99
-
-r = random.random()         # hasil antara 0 dan 1
-biased_r = r ** 5           # bikin angka tinggi makin jarang
-value = low + (high - low) * biased_r
-
-print(value)
